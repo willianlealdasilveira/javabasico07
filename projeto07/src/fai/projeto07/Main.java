05f6d52bfe1031eeafa93def03be58787d63a54c
WILLIAN <willianlealdasiveira@outlook.com.br>
2020-03-07 17:19:11 -0300

    primeiro commit

---
+++ projeto07/src/fai/projeto07/Main.java
@@ -0,0 +1,21 @@
+package fai.projeto07;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		 app.adcao();
+		 app.subtracao();
+		 app.concatenarstring();
+
+
+	}
+	
+		private void adcao() {}
+		private void subtracao() {}
+		private void concatenarstring() {}
+		
+		
+	}
+	
+
